5a88fcd77294991a90f9fc8de512d36666cd039a
Carla <107011235+carladfb@users.noreply.github.com>
2023-10-17 12:57:07 -0300

    Add files via upload
    Estudo sobre Values

---
+++ dio-values/src/main/java/dio/values/DioValuesApplication.java
@@ -0,0 +1,13 @@
+package dio.values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioValuesApplication.class, args);
+	}
+
+}
---
+++ dio-values/src/test/java/dio/values/DioValuesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioValuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
